a152f2a65924cb53f08d6d38d00527db0e420f3a
Subham Das <das.subham2001@gmail.com>
2024-06-21 01:07:32 +0530

    added the api

---
+++ script.js
@@ -0,0 +1,16 @@
+const jokeConatiner = document.getElementById("joke")
+const btn = document.getElementById("btn")
+
+const url = " https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = () => {
+    jokeConatiner.classList.remove("fade");
+    fetch(url)
+        .then(data => data.json())
+        .then(item => {
+            jokeConatiner.textContent = `${item.joke}`
+            jokeConatiner.classList.add("fade")
+        });
+}
+
+btn.addEventListener("click", getJoke)
